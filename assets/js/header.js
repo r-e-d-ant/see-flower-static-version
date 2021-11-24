a33949f6f1f205cb0e0ffde9dd1418e573bf98bf
r-e-d-ant <mugishathierry65@gmail.com>
2021-11-24 13:28:06 +0200

    header full styled and responsive, with javascript to toggle navbar on mobile view

---
+++ assets/js/header.js
@@ -0,0 +1,27 @@
+
+/* -- show and hide the navigation bar on mobile view -- */
+
+
+/* -- accessing the dom -- */
+const navBar = document.querySelector('.nav-bar');
+const openMenuIcon = document.querySelector('.open-menu-icon');
+const closeMenuIcon = document.querySelector('.close-menu-icon')
+// -------------------------
+
+/* -- open nav bar -- */
+openMenuIcon.addEventListener('click', () => {
+    if(!navBar.classList.contains('show-nav-bar')) {
+        navBar.classList.add('show-nav-bar');
+        openMenuIcon.classList.add('hide-open-menu__icon');
+        closeMenuIcon.classList.add('show-close-menu__icon');
+    }
+});
+
+/* -- close nav bar -- */
+closeMenuIcon.addEventListener('click', () => {
+    if(navBar.classList.contains('show-nav-bar')) {
+        navBar.classList.remove('show-nav-bar');
+        openMenuIcon.classList.remove('hide-open-menu__icon');
+        closeMenuIcon.classList.remove('show-close-menu__icon');
+    }
+});
